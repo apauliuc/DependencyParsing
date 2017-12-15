35af25ca9b3ea25a208291d6f937e76514b962ae
andrei sili <andrei.sili1994@gmail.com>
2017-12-15 03:58:12 +0100

    removed deugging slicing

--- src/BiLSTM.py
+++ src/BiLSTM.py
@@ -320,8 +320,8 @@ if __name__ == '__main__':
         loss_function.cuda()
 
     if args.language == 'en':
-        conllu_sentences_train = em.en_train_sentences()[0:50]
-        conllu_sentences_dev = em.en_dev_sentences()[0:20]
+        conllu_sentences_train = em.en_train_sentences()
+        conllu_sentences_dev = em.en_dev_sentences()
     elif args.language == 'ro':
         conllu_sentences_train = em.ro_train_sentences()
         conllu_sentences_dev = em.ro_dev_sentences()
